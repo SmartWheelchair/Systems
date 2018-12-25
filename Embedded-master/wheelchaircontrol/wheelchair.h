6fa2bb669edf7472605074a9fd48217a9d3003a2
jesim272 <45446341+jesim272@users.noreply.github.com>
2018-12-24 21:14:02 -0800

    Update wheelchair.h

--- Embedded-master/wheelchaircontrol/wheelchair.h
+++ Embedded-master/wheelchaircontrol/wheelchair.h
@@ -1,7 +1,7 @@
 #ifndef wheelchair
 #define wheelchair
 
-//Importing libraries into wheelchair.h
+/* Importing libraries into wheelchair.h */
 #include "chair_BNO055.h"
 #include "PID.h"
 #include "QEI.h"
@@ -11,21 +11,22 @@
 
 
 /*
-* joystick has analog out of 200-700, scale values between 1.3 and 3.3
+* Joystick has analog out of 200-700, scale values between 1.3 and 3.3
 */             
 #define def (2.5f/3.3f)                 //Default axis on joystick to stay neutral; used on x and y axis
 #define high 3.3f/3.3f                  //High power on joystick; used on x and y axis
 #define low (1.7f/3.3f)                 //Low power on joystick; used on x and y axis
-#define offset .035f                  //Joystick ajustment to be able to go strait. Chair dependent on manufactoring presision
+#define offset .035f                    //Joystick adjustment to be able to go straight. Chair dependent on manufactoring precision
 #define process .1                      //Defines default time delay in seconds
 
-//Pin plug in for Nucleo-L432KC
-#define xDir D9                         //PWM Pins
+/* Pin plug in for Nucleo-L432KC */
+#define xDir D9                         //* PWM Pins */
 #define yDir D10
-#define Encoder1 D7                     //Digital In Pull Up Pin
+#define Encoder1 D7                     //*Digital In Pull Up Pin */
 #define Encoder2 D8
-
 #define Diameter 31.75                  //Diameter of encoder wheel
+
+
 /** Wheelchair class
  * Used for controlling the smart wheelchair
  */
@@ -38,72 +39,76 @@ public:
      */
     Wheelchair(PinName xPin, PinName yPin, Serial* pc, Timer* time, QEI* wheel, QEI* wheelS);
     
-    /** move using the joystick */
+    /** Move using the joystick */
     void move(float x_coor, float y_coor);
     
-    /* turn right a certain amount of degrees using PID*/
+    /* Turn right a certain amount of degrees using PID*/
     void pid_right(int deg);
     
-    /* turn left a certain amount of degrees using PID*/
+    /* Turn left a certain amount of degrees using PID*/
     void pid_left(int deg);
     
-    /* drive the wheelchair forward */
+    /* Drive the wheelchair forward */
     void forward();
     
-    /* drive the wheelchair backward*/
+    /* Drive the wheelchair in reverse*/
     void backward();
     
-    /* turn the wheelchair right*/
+    /* Turn the wheelchair right*/
     void right();
     
-    /* turn the wheelchair left*/
+    /* Turn the wheelchair left*/
     void left();
     
-    /* stop the wheelchair*/
+    /* Stop the wheelchair*/
     void stop();
     
-    /* function to get imu data*/
+    /* Functions to get IMU data*/
     void compass_thread();
     void velosity_thread();
     void rosCom_thread();
     
-    /* move x millimiters foward using PID*/
+    /* Move x millimiters foward using PID*/
     void pid_forward(double mm);
     
+    /* Obtain angular position for Twist message */
     double getTwistZ();
     
-    /*  gets the encoder distance moved since encoder reset*/
+    /*  Gets the encoder distance moved since encoder reset*/
     float getDistance();
     
-    /* resets encoder*/
+    /* Resets encoder*/
     void resetDistance();
     
-    /* function to to determine whether we are turning left or right*/
+    /* Function to determine whether we are turning left or right*/
     void pid_turn(int deg);
     
+    /* Function to obtain angular and linear velocity */
     void pid_twistA();
     void pid_twistV();
     
+    /* Function to publish and show Odometry message */
     void odomMsg();
     void showOdom();
     
-    /* functions with a predetermined path demmo*/
+    /* Functions with a predetermined path (demo) */
     void desk();
     void kitchen();
     void desk_to_kitchen();
     
+    /* Variables for postion, angle, and velocity */
     double x_position;
     double y_position;
     double z_angular;   
-    double odom_vector[3];
     double curr_vel;
     double z_twistA;
     double linearV;
     double angularV;
     double vel;
-    double test1, test2;    
+    double test1, test2;  
+    
 private:
-    /* Pointers for the joystick speed*/
+    /* Pointers for the joystick speed */
     PwmOut* x;
     PwmOut* y;
     
@@ -111,6 +116,6 @@ private:
     Serial* out;                        // Pointer to Serial Monitor
     Timer* ti;                          // Pointer to the timer
     QEI* wheel;                         // Pointer to encoder
-    QEI* wheelS;                         // Pointer to encoder
+    QEI* wheelS;                        // Pointer to encoder
 };
 #endif
